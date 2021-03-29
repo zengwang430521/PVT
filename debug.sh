273f51e77d99777417fa58bd7ba6652a3ab55e3e
zengwang <zengwang430521@gmail.com>
2021-03-29 17:12:18 +0800

    timm

--- debug.sh
+++ debug.sh
@@ -15,11 +15,11 @@ srun -p $node \
 
 
 
-srun -p HA_3D --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=pvt --kill-on-bad-exit=1
+srun -p pat_earth --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=pvt --kill-on-bad-exit=1
 python main_for_debug.py --model=pvt_small --batch-size=1 --output_dir=data/models/ --data-path=/path/to/imagenet --resume=data/models/pvt_small_le1.5.pth --eval
 
 
-srun -p HA_3D --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=pvt --kill-on-bad-exit=1
+srun -p pat_earth --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=pvt --kill-on-bad-exit=1
 sh dist_train.sh pvt_small 8 logs/pvt_small --data-path /mnt/lustre/zengwang/data/imagenet
 
 
--- slurm_train.sh
+++ slurm_train.sh
@@ -19,4 +19,4 @@ srun -p ${PARTITION} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python -u main.py --launcher="slurm" ${PY_ARGS}
+    python -u main_2.py --launcher="slurm" ${PY_ARGS}
