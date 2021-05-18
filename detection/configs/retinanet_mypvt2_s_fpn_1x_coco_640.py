6f52c9107667c0bc3d0d0f717d6f58cc14c0c460
zengwang <zengwang430521@gmail.com>
2021-05-18 17:04:36 +0800

    debug

--- detection/configs/retinanet_mypvt2_s_fpn_1x_coco_640.py
+++ detection/configs/retinanet_mypvt2_s_fpn_1x_coco_640.py
@@ -59,11 +59,10 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    # samples_per_gpu=2,
-    # workers_per_gpu=2,
-
-    samples_per_gpu=1,
-    workers_per_gpu=1,
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    # samples_per_gpu=1,
+    # workers_per_gpu=1,
 
     train=dict(
         type=dataset_type,
