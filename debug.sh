4e5f706e46315d36f21eaf3d5f7563c0e236b27f
zengwang <zengwang430521@gmail.com>
2021-07-07 11:44:54 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -284,6 +284,12 @@ srun -p pat_earth \
     -x SH-IDC1-10-198-4-[100-103,116-119] \
     --job-name=pvt --ntasks=16 \
     --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
+      python -u train.py --model mypvt24_small --batch-size 64 --epochs 300 --num_workers 5  --cache_mode \
+    --output_dir work_dirs/my24 --data-path data/imagenet \
+    --input-size 448 \
+    --resume work_dirs/my24/checkpoint.pth
+
+
     python -u train.py --model mypvt23_small --batch-size 64 --epochs 300 --num_workers 5  --cache_mode \
     --output_dir work_dirs/my23_2 --data-path data/imagenet \
     --input-size 448 \
