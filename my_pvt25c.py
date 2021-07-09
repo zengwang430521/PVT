2d4530b319dece10948fe8fb7d96e68707bbef94
zengwang <zengwang430521@gmail.com>
2021-07-09 16:00:35 +0800

    debug

--- my_pvt25c.py
+++ my_pvt25c.py
@@ -757,7 +757,7 @@ def show_conf(conf, loc):
         ax.imshow(conf_map[0, 0].detach().cpu())
 
 
-class MyPVT25b(nn.Module):
+class MyPVT25c(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dims=[64, 128, 256, 512],
                  num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
                  attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
@@ -946,8 +946,8 @@ class MyPVT25b(nn.Module):
 
 
 @register_model
-def mypvt25b_small(pretrained=False, **kwargs):
-    model = MyPVT25b(
+def mypvt25c_small(pretrained=False, **kwargs):
+    model = MyPVT25c(
         patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], **kwargs)
     model.default_cfg = _cfg()
@@ -958,7 +958,7 @@ def mypvt25b_small(pretrained=False, **kwargs):
 # For test
 if __name__ == '__main__':
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model = mypvt25b_small(drop_path_rate=0.1).to(device)
+    model = mypvt25c_small(drop_path_rate=0.1).to(device)
     model.reset_drop_path(0.1)
 
     empty_input = torch.rand([2, 3, 448, 448], device=device)
