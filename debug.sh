46a9cec2de6d2e21fdb6109ded196638ed73eacd
zengwang <zengwang430521@gmail.com>
2021-07-09 15:42:03 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -263,10 +263,10 @@ spring.submit arun \
 srun -p 3dv-share \
     --job-name=test --ntasks=16 \
     --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
-    python -u train.py --model mypvt23__small --batch-size 64 --epochs 300 --num_workers 5  --cache_mode \
-    --output_dir work_dirs/my26a --data-path data/imagenet \
+    python -u train.py --model mypvt23_small --batch-size 64 --epochs 300 --num_workers 5  --cache_mode \
+    --output_dir work_dirs/my23_mlpres --data-path data/imagenet \
     --input-size 448 \
-    --resume work_dirs/my26a/checkpoint.pth
+    --resume work_dirs/my23_mlpres/checkpoint.pth
 
 
 
