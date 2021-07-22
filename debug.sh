76825fadfd63f29927409e104aac5ef6f3cfc3b4
zengwang <zengwang430521@gmail.com>
2021-07-22 16:38:14 +0800

    debug

--- debug.sh
+++ debug.sh
@@ -289,13 +289,24 @@ sh dist_train.sh pvt_small 8 ./work_dirs/debug --data-path data/imagenet --use-m
 
 
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
-srun -p 3dv-share  -w SH-IDC1-10-198-6-129\
+
+srun -p 3dv-share  -w SH-IDC1-10-198-6-138\
     --job-name=test --ntasks=8 \
     --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=4 --kill-on-bad-exit=1 \
+    python -u train.py --model mypvt25g_small --batch-size 64 --epochs 50 --num_workers 5 \
+    --output_dir work_dirs/debug --data-path data/imagenet \
+    --input-size 448 --resume work_dirs/my2520/checkpoint.pth --eval --use-mcloader
+
+
+    python -u train_finetune.py --model mypvt25g_small --batch-size 64 --epochs 50 --num_workers 5 \
+    --output_dir work_dirs/my25g_f --data-path data/imagenet \
+    --input-size 448 --resume work_dirs/my25g_f/checkpoint.pth \
+    --warmup-epochs 1 --cooldown-epochs 5 --fine_factor=0.1 \
+    --finetune work_dirs/my2520/checkpoint.pth --use-mcloader
+
     python -u train.py --model mypvt2520_2_small --batch-size 64 --epochs 50 --num_workers 5 \
     --output_dir work_dirs/debug --data-path data/imagenet \
-    --input-size 448 --resume work_dirs/my2520/checkpoint.pth --eval
-
+    --input-size 448 --resume work_dirs/my2520/checkpoint.pth --eval --use-mcloader
 
 
     python -u train_finetune.py --model mypvt25f_small --batch-size 64 --epochs 50 --num_workers 5 \
--- my_pvt25g.py
+++ my_pvt25g.py
@@ -900,7 +900,7 @@ def show_conf(conf, loc):
         ax.imshow(conf_map[0, 0].detach().cpu())
 
 
-class MyPVT25f(nn.Module):
+class MyPVT25g(nn.Module):
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dims=[64, 128, 256, 512],
                  num_heads=[1, 2, 4, 8], mlp_ratios=[4, 4, 4, 4], qkv_bias=False, qk_scale=None, drop_rate=0.,
                  attn_drop_rate=0., drop_path_rate=0., norm_layer=nn.LayerNorm,
@@ -1087,8 +1087,8 @@ class MyPVT25f(nn.Module):
 
 
 @register_model
-def mypvt25f_small(pretrained=False, **kwargs):
-    model = MyPVT25f(
+def mypvt25g_small(pretrained=False, **kwargs):
+    model = MyPVT25g(
         patch_size=4, embed_dims=[64, 128, 320, 512], num_heads=[1, 2, 5, 8], mlp_ratios=[8, 8, 4, 4], qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 4, 6, 3], sr_ratios=[8, 4, 2, 1], **kwargs)
     model.default_cfg = _cfg()
@@ -1106,7 +1106,7 @@ if __name__ == '__main__':
 
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model = mypvt25f_small(drop_path_rate=0.1).to(device)
+    model = mypvt25g_small(drop_path_rate=0.1).to(device)
     model.reset_drop_path(0.1)
 
     empty_input = torch.rand([2, 3, 448, 448], device=device)
