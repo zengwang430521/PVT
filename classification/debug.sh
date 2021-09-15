f40240537a7fd248802f00fadb96f18e7b01091d
zengwang <zengwang430521@gmail.com>
2021-09-16 02:12:46 +0800

    '3c2'

--- classification/debug.sh
+++ classification/debug.sh
@@ -3,6 +3,10 @@
 export NCCL_LL_THRESHOLD=0
 python -m torch.distributed.launch --nproc_per_node=8 --master_port=6333 --use_env \
     train.py --config configs/pvt_v2/debug.py \
+    --model=mypvt3c2_small --output_dir=work_dirs/my3c2_LR \
+    --batch-size 128 --data-path data/imagenet --input-size 112
+
+    train.py --config configs/pvt_v2/debug.py \
     --model=mypvt3b_small --output_dir=work_dirs/my3b_LR \
     --batch-size 128 --data-path data/imagenet --input-size 112
 
