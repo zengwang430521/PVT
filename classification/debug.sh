6d561560314e7fe5745c7af00a558121ea37762f
zengwang <zengwang430521@gmail.com>
2021-10-06 21:54:39 +0800

    debug

--- classification/debug.sh
+++ classification/debug.sh
@@ -9,11 +9,14 @@ train.py --config configs/pvt_v2/debug.py \
 
 srun -p 3dv-share  -w SH-IDC1-10-198-6-129\
 srun -p mmpose \
-srun -p pat_earth \
 srun -p mm_human \
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
+srun -p pat_earth \
     --job-name=pvt --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     python -u train.py --config configs/pvt_v2/debug.py \
+    --model=mypvt3h2_small --output_dir=work_dirs/debug \
+    --batch-size 128 --data-path data/imagenet --input-size 112 --use-mcloader
+
     --model=mypvt3h4_small --output_dir=work_dirs/my3h4_LR \
     --batch-size 128 --data-path data/imagenet --input-size 112 --use-mcloader --resume work_dirs/my3h4_LR/checkpoint.pth
 
