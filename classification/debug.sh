92a0b4069d964fc15729dd5292351d382bf8c75d
zengwang <zengwang430521@gmail.com>
2021-11-08 17:19:39 +0800

    debug

--- classification/debug.sh
+++ classification/debug.sh
@@ -24,6 +24,9 @@ export NCCL_LL_THRESHOLD=0
 python -m torch.distributed.launch --nproc_per_node=8 --master_port=6333 --use_env \
 train.py --config configs/pvt_v2/debug.py \
     --batch-size 128 --data-path data/imagenet --input-size 112  \
+    --model=myhrpvt_32_sr --output_dir=work_dirs/myhrpvt_32_sr_LR --resume work_dirs/myhrpvt_32_sr_LR/checkpoint.pth
+
+    --batch-size 128 --data-path data/imagenet --input-size 112  \
     --model=myhrpvt_32_re --output_dir=work_dirs/myhrpvt_32_re_LR --resume work_dirs/myhrpvt_32_re_LR/checkpoint.pth
 
 
@@ -47,6 +50,9 @@ srun -p mm_human \
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
     --job-name=pvt --ntasks=16 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     python -u train.py --config configs/pvt_v2/debug.py \
+    --batch-size 64 --data-path data/imagenet --input-size 224 --use-mcloader \
+    --model=myhrpvt_32 --output_dir=work_dirs/myhrpvt_32_16 --resume work_dirs/myhrpvt_32_16/checkpoint.pth
+
     --batch-size 64 --data-path data/imagenet --input-size 224 --use-mcloader \
     --model=mypvt3h2_density0f_large --output_dir=work_dirs/my3h2_density0f_large_16 --resume work_dirs/my3h2_density0f_large_16/checkpoint.pth
 
