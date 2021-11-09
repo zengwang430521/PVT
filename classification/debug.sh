64b5c2338e7c8f6e00647c90b246e449684c55dc
zengwang <zengwang430521@gmail.com>
2021-11-09 23:19:40 +0800

    debug

--- classification/debug.sh
+++ classification/debug.sh
@@ -18,7 +18,7 @@ export MASTER_PORT=29505
 srun -p mm_human \
 srun -p mm_human --quotatype=auto\
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
-    --job-name=fine --ntasks=4 --gres=gpu:4 --ntasks-per-node=4 --cpus-per-task=5 --kill-on-bad-exit=1 \
+    --job-name=fine --ntasks=4 --gres=gpu:4 --ntasks-per-node=4 --cpus-per-task=2 --kill-on-bad-exit=1 \
     python -u train.py --config configs/pvt_v2/debug.py \
     --batch-size 64 --data-path data/imagenet --input-size 224 --use-mcloader \
     --model=myhrpvt_32 --output_dir=work_dirs/hrpvt_fine --resume=work_dirs/hrpvt_fine/checkpoint.pth\
