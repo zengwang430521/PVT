c28d8853706b4d0bd6dcf5ba73e093bc3727555b
zengwang <zengwang430521@gmail.com>
2021-11-25 21:50:29 +0800

    debug

--- classification/debug.sh
+++ classification/debug.sh
@@ -134,6 +134,7 @@ srun -p pat_earth  \
 srun -p 3dv-share  -w SH-IDC1-10-198-6-129\
 srun -p mm_human \
 srun -p pat_earth -x SH-IDC1-10-198-4-[90-91,100-103,116-119] \
+srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
     --job-name=pvt --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     python -u train.py --config configs/pvt_v2/debug.py \
     --batch-size 128 --data-path data/imagenet --input-size 112 --use-mcloader \
