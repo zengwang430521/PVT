86624c8e1097f48f4e8d764ace957c814cf83637
zengwang <zengwang430521@gmail.com>
2021-12-16 23:46:05 +0800

    debug

--- classification/tc_module/tcformer_partpad.py
+++ classification/tc_module/tcformer_partpad.py
@@ -10,7 +10,7 @@ from .tcformer_utils import (
 from .transformer_utils import trunc_normal_
 # from .ctm_block import CTM as CTM
 from .ctm_block import CTM_partpad as CTM
-from mmdet.models.builder import BACKBONES
+# from mmdet.models.builder import BACKBONES
 from mmdet.utils import get_root_logger
 from mmcv.runner import load_checkpoint
 from timm.models.registry import register_model
