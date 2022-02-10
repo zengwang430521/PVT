a751dfb38f08b248a8fd2baf5f1ec6e9589193ae
zengwang <zengwang430521@gmail.com>
2022-02-10 23:27:42 +0800

    debug

--- detection/debug.sh
+++ detection/debug.sh
@@ -24,10 +24,11 @@ srun -p mm_human \
 srun -p mm_human --quotatype=auto\
 srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
    --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
-   --job-name=det python -u train.py --launcher="slurm" \
+   --job-name=det_2 python -u train.py --launcher="slurm" \
+    configs/mask_rcnn_tc_partpad_mta_1x_coco_2.py  --work-dir=work_dirs/mask_partpad_2
+
    configs/mask_rcnn_tc_partpad_hr_1x_coco.py --work-dir=work_dirs/mask_partpad_hr
 
-    configs/mask_rcnn_tc_partpad_mta_1x_coco_2.py  --work-dir=work_dirs/mask_partpad_2
 
 
    --job-name=det python -u train.py configs/mask_rcnn_tc_partpad_bimta_1x_coco.py \
