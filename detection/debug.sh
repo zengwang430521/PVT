452fbb3a8f7622fca14b12669d0fc8f0d2e574ec
zengwang <zengwang430521@gmail.com>
2022-02-14 10:19:49 +0800

    'hr2'

--- detection/debug.sh
+++ detection/debug.sh
@@ -21,8 +21,8 @@ srun -p pat_earth  \
 srun -p pat_earth -x SH-IDC1-10-198-4-[90-91,100-103,116-119] \
 srun -p mm_human \
 srun -p mm_human --quotatype=auto\
-srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
 srun -p pat_earth  \
+srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
   --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
    --job-name=det python -u train.py --launcher="slurm" \
    configs/mask_rcnn_tc_partpad_hr2_1x_coco.py --work-dir=work_dirs/mask_partpad_hr2
